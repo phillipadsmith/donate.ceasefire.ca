5f045771a3b3f4ce42a9169fe91205ae2e0853f3
Phillip Smith <ps@phillipadsmith.com>
2014-02-10 21:54:30 -0600

    Many updates: validation, plus tablet and desktop layouts

--- ui/js/site.js
+++ ui/js/site.js
@@ -3,5 +3,44 @@ $(document).ready(function() {
     $('#billing-different').click(function() {
         $('fieldset#billing-information').toggle();
     });
+
     $('[rel="popover"]').popover();
+
+
+    // override jquery validate plugin defaults
+    $.validator.setDefaults({
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
+    $("#payment_form").validate({
+        debug: true,
+        success: "valid",
+        rules: {
+            email_addr: "required",
+            email_addr2: {
+                equalTo: "#email_addr",
+
+            }
+        },
+        messages: {
+            name: "Please specify your name",
+            email_addr2: {
+                equalTo: "The email addresses must match",
+            }
+        }
+    });
 });

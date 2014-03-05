a5ab4275109faa056f6debbd0c6e281caf54e37e
Phillip Smith <ps@phillipadsmith.com>
2014-03-05 14:38:21 -0600

    First pass at a donation amount confirmation box

--- ui/js/site.js
+++ ui/js/site.js
@@ -6,16 +6,46 @@ $(document).ready(function() {
 
     // control recurring vs. one-time contributions
     $('select.frequency').change((function() {
+        show_widget_amount_and_frequncy();
         var frequency = $("select.frequency option:selected").attr("value");
         if (frequency === 'onetime') {
             $('.onetime').show();
             $('input[name="send_me_gift"]').attr("checked", false);
             $('.sustaining').hide();
+            amount = $('input[name="amount_1"]').val();
         } else if ($('.sustaining').is(":hidden")) {
             $('.sustaining').show();
             $('.onetime').hide();
+            amount = $('input[name="amount_r"]').val();
         }
     }));
+    $('input[name="amount_r"]').blur((function() {
+        console.log('amount_r blur');
+        show_widget_amount_and_frequncy();
+    }));
+    $('input[name="amount_1"]').blur((function() {
+        console.log('amount_1 blur');
+        show_widget_amount_and_frequncy();
+    }));
+
+
+    function show_widget_amount_and_frequncy() {
+        var amount;
+        var frequency_text = $("select.frequency option:selected").text();
+        var frequency = $("select.frequency option:selected").val();
+        if (frequency === 'onetime') {
+            amount = $('input[name="amount_1"]').val();
+        } else {
+            amount = $('input[name="amount_r"]').val();
+        }
+        $('.widget_amount').text(amount);
+        $('.widget_frequency').text(frequency_text);
+        if (amount !== '') {
+            $('.widget_amount_frequency').show();
+        }
+    }
+
+    // For Paypal tab, but could be merged with previous function
     $('select.frequency_paypal').change((function() {
         var frequency = $("select.frequency_paypal option:selected").attr("value");
         if (frequency === 'onetime') {

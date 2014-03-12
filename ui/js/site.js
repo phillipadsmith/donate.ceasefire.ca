4d5834532b51e58b894813f5f3cfe1f0f0bb2eea
Phillip Smith <ps@phillipadsmith.com>
2014-03-12 16:56:30 -0600

    Success page (success.html). Auto-filling in form fields and elements with purl.js to demonstrate the user flow

--- ui/js/site.js
+++ ui/js/site.js
@@ -14,6 +14,37 @@ $(document).ready(function() {
         });
         $('.donors ul').append(items);
     });
+    // Use purl.js to show query parameters
+    var url = $.url(); // parse the current page URL
+    var first_name = url.param('first_name');
+    var last_name = url.param('last_name');
+    var full_name = first_name + ' ' + last_name;
+    var email = url.param('email_addr');
+    var amount = url.param('amount_r') || url.param('amount_1');
+    var day_of_month = url.param('day_of_month');
+    var frequency;
+    if (day_of_month === '1') {
+        frequency = 'each month on the 1st.';
+    } else if (day_of_month === '15') {
+        frequency = 'each month on the 15th.';
+    } else if (day_of_month === 'w') {
+        frequency = 'each week.';
+    } else {
+        frequency = 'one time.';
+    }
+
+    if (url.attr('path') === '/success.html') {
+        $('dd.trn-name').text(full_name);
+        $('dd.trn-email').text(email);
+        $('dd.trn-details span.trn-amount').text(amount);
+        $('dd.trn-details span.trn-frequency').text(frequency);
+    } else {
+        $('input[name="amount_r"]').val(amount);
+        $('input[name="first_name"]').val(first_name);
+        $('input[name="last_name"]').val(last_name);
+        $('input[name="email_addr"]').val(email);
+    }
+
 
     // control recurring vs. one-time contributions
     $('select.frequency').change((function() {
